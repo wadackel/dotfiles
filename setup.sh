a7ecd5b500b5055739598beca36bb9e863c519c5
tsuyoshiwada <v23.b.wada@gmail.com>
2017-03-15 13:26:57 +0900

    Add hyper configure

--- setup.sh
+++ setup.sh
@@ -6,3 +6,4 @@ ln -sf ~/dotfiles/gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/vimrc ~/.vimrc
 ln -sf ~/dotfiles/zshrc ~/.zshrc
+ln -sf ~/dotfiles/hyper.js ~/.hyper.js
