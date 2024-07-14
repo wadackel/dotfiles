12ad0c85bdc3a4f37398c025d0cf0f1c0fd9e159
wadackel <wadackel@gmail.com>
2024-07-14 22:43:42 +0900

    Update

--- init.lua
+++ init.lua
@@ -1578,6 +1578,7 @@ require("lazy").setup({
 
     {
       "nvim-telescope/telescope.nvim",
+      event = "VeryLazy",
       dependencies = {
         "nvim-lua/plenary.nvim",
         { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
